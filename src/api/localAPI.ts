194a48b7d3bc24a61512c1c3c20190f7a3acd416
Briws <brightwiths@gmail.com>
2022-02-21 23:56:50 +0300

    refactoring

--- src/api/localAPI.ts
+++ src/api/localAPI.ts
@@ -4,7 +4,14 @@ import BepHydro from '../assets/abis/bephydro.json';
 import {AbiItem} from "web3-utils";
 
 export const localAPI = {
-    connectToMetamask: async () => {
+    getAccountAddress: async () => {
+        const accounts = await window.web3.eth.getAccounts();
+        return accounts[0]
+    },
+    getNetworkID: async () => {
+        return await window.web3.eth.net.getId();
+    },
+    connectToMetamask: async function() {
         let returnValues = {
             status: false,
             account: '',
@@ -16,28 +23,21 @@ export const localAPI = {
                 window.web3 = new Web3(window.ethereum); // Instance web3 with the provided information
             }
             if (typeof window.web3 !== 'undefined') {
-                console.log('Web3 Detected!')
                 window.web3 = new Web3(window.web3.currentProvider);
-                returnValues.account = await localAPI.getAccount()
+                returnValues.account = await this.getAccountAddress()
             } else {
                 console.log('No Web3 Detected')
                 window.web3 = new Web3(new Web3.providers.WebsocketProvider('wss://infura.io/ws/v3/72e114745bbf4822b987489c119f858b'));
             }
             returnValues.status = true
         } catch (error) {
-            console.warn('localAPI.connectToMetamask error')
+            console.error('localAPI.connectToMetamask error')
         }
 
-        returnValues.networkID = await localAPI.getNetworkID()
+        returnValues.networkID = await window.web3.eth.net.getId()
         return returnValues
     },
-    getAccount: async () => {
-        const accounts = await window.web3.eth.getAccounts();
-        return accounts[0]
-    },
-    getNetworkID: async () => {
-        return await window.web3.eth.net.getId();
-    },
+
     changeNetwork: async (networkName: string) => {
         try {
             await window.ethereum.request({
@@ -63,6 +63,21 @@ export const localAPI = {
         const hydroAddress = "0xf3DBB49999B25c9D6641a9423C7ad84168D00071";
         return new web3.eth.Contract(BepHydro as AbiItem[], hydroAddress);
     },
+    getHydroBalance: async function() {
+        const address = await this.getAccountAddress()
+        console.log('account', address)
+        const balance = await window.web3.eth.getBalance(address)
+        const balanceTransformed =  window.web3.utils.fromWei(balance)
+        console.log('balanceEther', balanceTransformed) //this is ether balance
+        // now getting token balance
+        const tokenInst = new window.web3.eth.Contract(BepHydro, address);
+        console.log('hydroInstance', tokenInst)
+        try {
+            const HydroInstance = await tokenInst.methods.balanceOf(address).call()
+        } catch (error) {
+            console.error(error)
+        }
+    }
 }
 
 declare let window: any // todo: maybe fix any
--- src/redux/bridge-reducer.ts
+++ src/redux/bridge-reducer.ts
@@ -1,9 +1,7 @@
 import {ThunkAction} from "redux-thunk";
 import {AppRootStateType} from "./store";
-import Web3 from "web3";
 import {Contract} from "web3-eth-contract";
 import {fromWei} from "web3-utils";
-import {chains} from "../assets/chains";
 import {localAPI} from "../api/localAPI";
 
 let initialState = {
@@ -14,7 +12,6 @@ let initialState = {
     allowedBep: '0',
     loading: false,
     hydroInstance: {} as Contract,
-    // web3: {} as Web3,
     hydroAddress: null as string | null,
     bepHydroAddress: null,
     ethToBscInstance: null,
@@ -44,7 +41,6 @@ let initialState = {
 
 export const bridgeReducer = (state = initialState, action: BridgeActionTypes): InitialStateType => {
     switch (action.type) {
-        case 'BRIDGE/SET-WEB3':
         case 'BRIDGE/SET-NETWORK-ID':
         case 'BRIDGE/SET-LOADING':
         case "BRIDGE/SET-ACCOUNT":
@@ -54,7 +50,6 @@ export const bridgeReducer = (state = initialState, action: BridgeActionTypes): 
 }
 
 //Action creators:
-const setWeb3AC = (web3: Web3) => ({type: 'BRIDGE/SET-WEB3', payload: {web3}} as const)
 const setNetworkIDAC = (networkID: number) => ({type: 'BRIDGE/SET-NETWORK-ID', payload: {networkID}} as const)
 const setLoadingAC = (loading: boolean) => ({type: 'BRIDGE/SET-LOADING', payload: {loading}} as const)
 const setAccountAC = (account: string) => ({type: 'BRIDGE/SET-ACCOUNT', payload: {account}} as const)
@@ -76,72 +71,34 @@ export const connectToMetamaskThunk = (): AppThunk => async (dispatch, getState:
 
     //turn on monitoring if chain in metamask changed
     window.ethereum.on('chainChanged', async function () {
-        // const account = await getAccountHelper()
-        const account = await localAPI.getAccount()
+        const account = await localAPI.getAccountAddress()
         dispatch(setAccountAC(account))
 
-        // const networkID = await setNetworkIDHelper()
         const networkID = await localAPI.getNetworkID()
         dispatch(setNetworkIDAC(networkID))
-
-        console.log('CHAIN CHANGED!')
     })
 
 }
 
 export const changeNetworkThunk = (networkName: string): AppThunk => async (dispatch, getState: () => AppRootStateType) => {
     if (await localAPI.changeNetwork(networkName)) {
-        console.log('changeNetwork success')
-
         //todo: here will be status of app with progress bar
-
     } else {
-        console.log('changeNetwork success')
+        console.error('changeNetwork error')
     }
 }
 
 export const getHydroBalanceThunk = (): AppThunk => async (dispatch, getState: () => AppRootStateType) => {
-    /* const bridgeState = getState().bridge
-     const hydroInstance = localAPI.getHydroInstance(bridgeState.web3)
-     const account = await localAPI.getAccount()
-
-     hydroInstance.events.allEvents({
-         filter: {owner: bridgeState.account},
-         fromBlock: 'latest',
-         toBlock: 'latest'
-     })
-
-     if (hydroInstance.methods) {
-         if (bridgeState.account) {
-             debugger
-             try {
-                 // const res = await hydroInstance.methods.balanceOf(bridgeState.account).call()
-                 // const hydroBalance = bridgeState.web3.utils.fromWei(res.toString(), 'ether');
-                 debugger
-             }
-             catch (error) {
-                 console.error(error)
-                 // debugger
-             }
-         }
-
-         // const hydroBalance = bridgeState.web3.utils.fromWei(res.toString(), 'ether');
-         // dispatch(setHydroBalanceAC(hydroBalance))
-     }*/
-
+    await localAPI.getHydroBalance()
 }
 
 export const approveFundsThunk = (): AppThunk => async (dispatch, getState: () => AppRootStateType) => {
-    const bridgeState = getState().bridge
-    await bridgeState.hydroInstance.methods.approve(bridgeState.swapAddress, window.web3.utils.toWei('1000000000')).send({
-        from: bridgeState.account,
-    })
+    alert('approveFundsThunk')
 }
 
 export type InitialStateType = typeof initialState
 
 export type BridgeActionTypes =
-    | ReturnType<typeof setWeb3AC>
     | ReturnType<typeof setNetworkIDAC>
     | ReturnType<typeof setLoadingAC>
     | ReturnType<typeof setAccountAC>
