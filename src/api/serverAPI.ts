fc40c407f372c4154b578aa069f45c805fd26874
Ayushya Chitransh <ayushya@projecthydro.org>
2022-06-28 19:29:02 +0530

    Update baseURL for heroku

--- src/api/serverAPI.ts
+++ src/api/serverAPI.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {ReceiptedType} from './localAPI'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api/1.0.0/',
+    baseURL: 'https://hydro-bridgev2.herokuapp.com/api/1.0.0/',
     // baseURL: 'https://hydro-bridge.org/api/1.0.0/',
     withCredentials: false,
 })
